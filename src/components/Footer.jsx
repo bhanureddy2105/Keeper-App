a05dbf711a15a89451fccabe71d3ca2f517477a1
bhanureddy2105 <bhanu.vangati@gmail.com>
2021-01-01 17:51:59 +0530

    Added Readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 
     var year=new Date().getFullYear()
     return <footer>
-        <p>copyright &copy; {year}</p>
+        <p>copyright &copy; {year} Bhanu</p>
     </footer>
  }
 
